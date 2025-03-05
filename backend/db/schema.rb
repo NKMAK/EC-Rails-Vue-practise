19dc6b20dea63c6d7331eb8268d6fbbc1a7ac11e
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-05 09:28:40 +0900

    schema作成

---
+++ backend/db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.2].define(version: 2025_03_04_096048) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "refresh_tokens", force: :cascade do |t|
+    t.uuid "user_uuid", null: false
+    t.string "token", null: false
+    t.datetime "expires_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_refresh_tokens_on_token", unique: true
+    t.index ["user_uuid"], name: "index_refresh_tokens_on_user_uuid"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.uuid "user_uuid", default: -> { "gen_random_uuid()" }, null: false
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.boolean "is_admin", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["user_uuid"], name: "index_users_on_user_uuid", unique: true
+  end
+
+  add_foreign_key "refresh_tokens", "users", column: "user_uuid", primary_key: "user_uuid"
+end
