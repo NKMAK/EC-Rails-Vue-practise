12043cc89ac090a3be4f3fa5586c962f20ba2bd4
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-05 09:40:53 +0900

    ユーザーとリフレッシュトークンモデルの関連付けを追加

--- backend/app/models/refresh_token.rb
+++ backend/app/models/refresh_token.rb
@@ -1,2 +1,3 @@
 class RefreshToken < ApplicationRecord
+  belongs_to :user, foreign_key: 'user_uuid', primary_key: 'user_uuid'
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password  # パスワードの暗号化と認証機能
   validates :email, presence: true, uniqueness: true  # メールアドレスのバリデーショ
+  has_one :refresh_token, foreign_key: 'user_uuid', primary_key: 'user_uuid'
 end
