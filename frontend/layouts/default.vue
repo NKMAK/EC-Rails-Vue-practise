de5416490b763fb973e8ba05f1accf8ff6f94fe8
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-08 17:50:13 +0900

    Vutify導入

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-app>
+    <div id="layout">
+      <p>test</p>
+      <v-main>
+        <slot />
+      </v-main>
+    </div>
+  </v-app>
+</template>
+
+<style scoped>
+header {
+  background: #333;
+  color: white;
+}
+footer {
+  background: #f4f4f4;
+  padding: 10px;
+  text-align: center;
+}
+</style>
