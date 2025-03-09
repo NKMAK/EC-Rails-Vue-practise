11ae04111811ba55a69e4e47fbdd6af9ac662f79
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-09 21:46:29 +0900

    ログイン・新規登録フォーム作成

---
+++ frontend/pages/auth/_components/LoginForm.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { LoginData } from "../_models/model";
+import { validationRules } from "../_function/validation";
+
+const emit = defineEmits<{
+  (e: "loginEvent", data: LoginData): void;
+}>();
+
+const email = ref("");
+const password = ref("");
+const showPassword = ref(false);
+
+const login = () => {
+  emit("loginEvent", {
+    email: email.value,
+    password: password.value,
+  });
+};
+</script>
+
+<template>
+  <v-card-text>
+    <v-form @submit.prevent="login">
+      <v-text-field
+        v-model="email"
+        label="メールアドレス"
+        prepend-icon="mdi-email"
+        type="email"
+        required
+        :rules="[validationRules.required, validationRules.email]"
+      />
+
+      <v-text-field
+        v-model="password"
+        label="パスワード"
+        prepend-icon="mdi-lock"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="showPassword ? 'text' : 'password'"
+        required
+        :rules="[validationRules.required]"
+        @click:append="showPassword = !showPassword"
+      />
+
+      <v-btn type="submit" color="primary" block class="mt-4"> ログイン </v-btn>
+    </v-form>
+  </v-card-text>
+</template>
---
+++ frontend/pages/auth/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import LoginForm from "./_components/LoginForm.vue";
+import SignupForm from "./_components/SignupForm.vue";
+import type { LoginData, SignupData } from "./_models/model";
+
+const tab = ref("login");
+
+const handleLogin = (loginData: LoginData) => {
+  console.log("ログイン処理", loginData);
+};
+
+const handleRegister = (registerData: SignupData) => {
+  console.log("新規登録処理", registerData);
+};
+</script>
+
+<template>
+  <v-row class="justify-center">
+    <v-col md="5" sm="9">
+      <v-card class="my-4 text-center">
+        <v-tabs v-model="tab">
+          <v-tab value="login">ログイン</v-tab>
+          <v-tab value="register">新規登録</v-tab>
+        </v-tabs>
+
+        <v-window v-model="tab">
+          <v-window-item value="login">
+            <LoginForm @loginEvent="handleLogin" />
+          </v-window-item>
+
+          <v-window-item value="register">
+            <SignupForm @signupEvent="handleRegister" />
+          </v-window-item>
+        </v-window>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
