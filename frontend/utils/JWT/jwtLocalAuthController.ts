c338bdab7005471ac72acb8fab29a50df92d63be
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-10 21:35:57 +0900

    クライアント側でJWTの期限が切れていた場合リフレッシュトークンの使用

--- frontend/utils/JWT/jwtLocalAuthController.ts
+++ frontend/utils/JWT/jwtLocalAuthController.ts
@@ -1,17 +1,28 @@
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode, type JwtPayload } from "jwt-decode";
 import { getAccessToken } from "../cokkie/getAccessToken";
+import { refreshToken } from "~/service/auth/refreshToken";
 
-export const jwtLocalAuthController = () => {
+interface CustomJwtPayload extends JwtPayload {
+  user_uuid: string;
+}
+
+export const jwtLocalAuthController = async () => {
   //トークンが有効であればtrueを返す
   const jwtToken = getAccessToken();
   if (jwtToken == null) return false;
-
+  console.log(jwtToken);
   try {
-    const decoded = jwtDecode(jwtToken);
+    const decoded = jwtDecode<CustomJwtPayload>(jwtToken);
+    console.log(decoded);
 
     if (decoded.exp == null) return false;
 
-    return Date.now() < decoded.exp * 1000;
+    if (Date.now() < decoded.exp * 1000) {
+      return true;
+    } else {
+      const resFlag = await refreshToken(decoded.user_uuid);
+      return resFlag;
+    }
   } catch (error) {
     console.error("JWTのデコードに失敗しました:", error);
     return false;
