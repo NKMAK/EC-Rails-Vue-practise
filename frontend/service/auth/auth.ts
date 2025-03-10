d2d45c0845e3995215604ac4636ff4d014d9e81c
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-10 18:08:35 +0900

    signup, login用のfetch関数作成

--- frontend/service/auth/auth.ts
+++ frontend/service/auth/auth.ts
@@ -1,4 +1,52 @@
-export const signup = async () => {
-  const response = await fetch("http://localhost:8000/up");
+import type { LoginData, SignupData } from "~/model/model";
+
+const baseUrl = "http://localhost:8000/api/"; //TODO envファイルに記述
+export const login = async (loginData: LoginData): Promise<boolean> => {
+  try {
+    const response = await fetch(baseUrl + "login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          email: loginData.email,
+          password: loginData.password,
+        },
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const signup = async (signupData: SignupData): Promise<boolean> => {
+  try {
+    const response = await fetch(baseUrl + "signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: signupData.name,
+          email: signupData.email,
+          password: signupData.password,
+        },
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const helthCheck = async () => {
+  const response = await fetch(baseUrl + "up");
   console.log(response);
 };
