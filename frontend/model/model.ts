aa5a32a038c83486c7106ee87fac1e26b1e5db0b
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-11 09:57:31 +0900

    商品テーブル登録用のモデル追加

--- frontend/model/model.ts
+++ frontend/model/model.ts
@@ -8,3 +8,11 @@ export interface SignupData {
   email: string;
   password: string;
 }
+
+export interface ProductData {
+  title: string;
+  description: string;
+  price: number;
+  stock_quantity: number;
+  active: boolean;
+}
