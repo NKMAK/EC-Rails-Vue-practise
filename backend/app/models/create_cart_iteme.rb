4c24eb40a3993bdd8e515b31fff79ff60827ff6d
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-16 12:25:20 +0900

    商品カートテーブルに関連するファイルの名前修正

--- backend/app/models/create_cart_iteme.rb
+++
@@ -1,2 +0,0 @@
-class CreateCartIteme < ApplicationRecord
-end
--- backend/test/models/cart_iteme_test.rb
+++ backend/test/models/cart_iteme_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class CreateCartItemeTest < ActiveSupport::TestCase
+class CartItemeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
