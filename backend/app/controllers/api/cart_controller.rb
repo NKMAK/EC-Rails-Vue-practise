54431b79a57b661d2494282a74ab90b3d1237bd4
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-17 14:09:32 +0900

    在庫以上にカートに商品を入れた時のエラー処理

--- backend/app/controllers/api/cart_controller.rb
+++ backend/app/controllers/api/cart_controller.rb
@@ -7,23 +7,26 @@ class Api::CartController < ApplicationController
     quantity = product_params[:quantity].to_i || 1
     product = Product.find(product_id)
     unless product
-      render json: { error: 'Product not found' }, status: :not_found 
+      return render json: { error: 'Product not found' }, status: :not_found 
     end
 
-    if product.stock_quantity < product_params [:quantity].to_i
-      render json: { error: 'Out of stock' }, status: :unprocessable_entity
-    end
+
 
     product_cart = @current_user.carts.find_or_create_by(active: true) # カートがなければ作成user_uuidも自動で紐づけ
     cart_item = product_cart.cart_items.find_by(product_id: product_id)
 
+
     if cart_item #update
+      if product.stock_quantity < cart_item.quantity + product_params [:quantity].to_i
+        return render json: { error: 'Out of stock' }, status: :unprocessable_entity
+      end
+
       cart_item.quantity += quantity
 
       if cart_item.save
-        render json:{sucess: true, text: "quanity change", cart: product_cart.as_json(include: :cart_items)}, status: :ok
+        return render json:{sucess: true, text: "quanity change", cart: product_cart.as_json(include: :cart_items)}, status: :ok
       else
-        render json: {error: cart_item.errors}, status: :unprocessable_entity
+        return render json: {error: cart_item.errors}, status: :unprocessable_entity
       end
     else #create
       cart_item = product_cart.cart_items.new(
@@ -33,9 +36,9 @@ class Api::CartController < ApplicationController
       )
 
       if cart_item.save
-        render json:{sucess: true, text: "new create", cart: product_cart.as_json(include: :cart_items)}, status: :ok
+        return render json:{sucess: true, text: "new create", cart: product_cart.as_json(include: :cart_items)}, status: :ok
       else
-        render json: {error: cart_item.errors}, status: :unprocessable_entity
+        return render json: {error: cart_item.errors}, status: :unprocessable_entity
       end
     end
   end
