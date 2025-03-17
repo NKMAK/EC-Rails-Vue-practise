fa1016cdc2bdf9e37c8e383cefd1c0a429646f7d
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-17 16:40:44 +0900

    /deleteCartItem カートから商品を削除し終わった後にカート内の合計金額再計算

--- backend/app/models/cart_item.rb
+++ backend/app/models/cart_item.rb
@@ -3,6 +3,7 @@ class CartItem < ApplicationRecord
   belongs_to :product, foreign_key: 'product_id', primary_key: 'id'
 
   after_save :update_cart_total
+  after_destroy :update_cart_total
 
   def update_cart_total
     cart.update(total_price: cart.total)
