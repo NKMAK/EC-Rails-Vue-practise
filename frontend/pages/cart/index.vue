780aefb98751bfbeb74a8d3f0132c1ada25f06a3
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-17 20:46:16 +0900

    /getCart fetchする関数作成

---
+++ frontend/pages/cart/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { gainCart } from "~/service/cart/gainCart";
+
+onMounted(async () => {
+  const test = await gainCart();
+  console.log(test);
+});
+</script>
+<template>test</template>
