1b3574df4b0d119748cc635fe8cb1eccf4bcd794
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-03-18 17:26:36 +0900

    topbar 変数名修正

--- frontend/components/TopBar.vue
+++ frontend/components/TopBar.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-const drawer = ref(false);
+const isDrawerRef = ref(false);
 </script>
 
 <template>
   <div>
     <v-app-bar :elevation="2">
       <template v-slot:prepend>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon
+          @click="isDrawerRef = !isDrawerRef"
+        ></v-app-bar-nav-icon>
       </template>
       <v-app-bar-title>
         <NuxtLink to="/home" class="text-decoration-none color-black"
@@ -14,18 +16,22 @@ const drawer = ref(false);
         >
       </v-app-bar-title>
     </v-app-bar>
+    <ClientOnly>
+      <v-navigation-drawer v-model="isDrawerRef" temporary>
+        <v-divider></v-divider>
 
-    <v-navigation-drawer v-model="drawer" temporary>
-      <v-divider></v-divider>
-
-      <v-list>
-        <v-list-item>
-          <NuxtLink to="/home">ホーム</NuxtLink>
-        </v-list-item>
-        <v-list-item>
-          <NuxtLink to="/product-regist">商品登録</NuxtLink>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+        <v-list>
+          <v-list-item>
+            <NuxtLink to="/home">ホーム</NuxtLink>
+          </v-list-item>
+          <v-list-item>
+            <NuxtLink to="/product-regist">商品登録</NuxtLink>
+          </v-list-item>
+          <v-list-item>
+            <NuxtLink to="/cart">カート</NuxtLink>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </ClientOnly>
   </div>
 </template>
